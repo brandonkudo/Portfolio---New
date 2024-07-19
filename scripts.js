0c9883defcf58b0ed262f9250d1bbb97786c0cb6
Brandon Kudo <brandonkudo@Brandons-MacBook-Pro.local>
2024-07-19 15:37:47 -0700

    Added javascript file, and styles for smaller screen sizes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function isScrolledIntoView(el) {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
@@ -10,9 +11,65 @@ function isScrolledIntoView(el) {
     return isVisible;
   }
 
+  function hamburgerMenu() {
+    const hamburger = document.getElementById("hamburger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    // const subMenus = document.getElementsByClassName("sub-menu");
+    // const parents = document.querySelectorAll('.mm-inner > ul > li');
+    // const bg = document.querySelector('.mm-bg');
+    // const body = document.querySelector('body');
+    const header = document.querySelector('#header');
+    let hamburgerOpen = false;
+    $(hamburger).click(function() {
+        console.log('jquery');
+    });
+    hamburger.addEventListener("click", () => {
+      if (!hamburgerOpen) {
+        // let subMenuTotals = 0;
+        // for (var i = 0; i < subMenus.length; i++) {
+        //   let scrollHeight = parseInt(subMenus[i].scrollHeight);
+        //   subMenuTotals = scrollHeight + subMenuTotals;
+        // }
+        hamburger.classList.add("open");
+        hamburgerOpen = true;
+        // mobileMenu.style.maxHeight =
+        //   mobileMenu.scrollHeight + subMenuTotals + "px";
+        $(mobileMenu).slideToggle();
+        // $(bg).slideToggle();
+        // body.style.overflow = 'hidden';
+        // $(header).toggleClass('open');
+      } else {
+        hamburger.classList.remove("open");3
+        hamburgerOpen = false;
+        // mobileMenu.style.maxHeight = null;
+        $(mobileMenu).slideToggle();
+        // $(bg).slideToggle();
+        // body.style.overflow = 'visible';
+        // $(header).toggleClass('open');
+
+
+      }
+    });
+
+    // parents.forEach(function(parent) {
+    //   let child = parent.querySelector('ul');
+    //   $(parent).append('<i class="fas fa-chevron-down"></i>');
+    //   let chevron = parent.querySelector('i');
+
+
+    //   parent.addEventListener('click',function() {
+    //     $(child).slideToggle();
+    //     $(chevron).toggleClass('fa-chevron-down');
+    //     $(chevron).toggleClass('fa-chevron-up');
+
+    //   });
+    // });
+}
+
   window.onload = function () {
     try {
-
+        hamburgerMenu();
+        console.log('js');
     } catch(e) {
         console.log(e);
     }
